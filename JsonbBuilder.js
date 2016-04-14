cf74f2b9645d98e7f7c7ad4a5ad8c6591957357f
dev <dev@dev.team.rfg.com.au>
2016-04-14 14:15:13 +1000

    init

---
+++ JsonbBuilder.js
@@ -0,0 +1,129 @@
+import { typeOf } from 'lutils'
+import Transposer from 'transposer'
+
+const transpose = Transposer.prototype.transpose
+
+export default class JsonbBuilder {
+    constructor(config = {}) {
+        this.column = config.column || ''
+    }
+
+    /**
+     *  Builds an array of jsonb query objects
+     *
+     *  @param    {Object}  config
+     *  @return   {Array of JsonbQuery}
+     */
+    get(input, inputSearch) {
+        let paramTrees = input
+
+        if ( typeOf.String(input) ) paramTrees = { [ input ]: inputSearch }
+
+        const queries = []
+        for ( let key in paramTrees ) {
+            // Casts the key into a proper tree if necessary
+            let pointer = transpose(key, paramTrees[key])
+
+            let query = new JsonbQuery({ column: this.column }).parse(pointer)
+
+            queries.push(query)
+        }
+
+        return typeOf.String(input) ? queries[0] : queries
+    }
+}
+
+export class JsonbQuery {
+    comparators = {
+        $eq: (value) => this._compare({ value, operator: '=' }),
+        $gt: (value) => this._compare({ value, operator: '>' }),
+        $lt: (value) => this._compare({ value, operator: '<' }),
+    };
+
+    types = {
+        'number': '::int',
+    }
+
+    constructor({ column } = {}) {
+        this.column = column || ''
+    }
+
+    parse(tree) {
+        let { keys, search } = this._parseTree(tree)
+
+        this.keys   = keys
+        this.search = search
+
+        return this
+    }
+
+    get({ asStatement = false, search = this.search } = {}) {
+        return this.buildQuery(this.keys, ( asStatement ? null : search ))
+    }
+
+    buildQuery(keys = this.keys, search) {
+        let items = []
+
+        if ( this.column )
+            keys.unshift(this.column)
+
+        let lastIndex = keys.length - 1
+        let parts     = keys.map((key, index) => {
+            // Ensures the first joiner is ignored, and the last is ->>
+            let joiner = index === 0
+                ? ''
+                : index === lastIndex ? ' ->> ' : ' -> '
+
+            return [ joiner, key ].join('')
+        })
+
+        if ( search ) {
+            for ( let key in search ) {
+                let value     = search[key]
+                let comparitor = this.comparators[key]
+
+                items.push(`( ${parts.join('')} ) ${ comparitor(value) }`)
+            }
+        } else {
+            items.push(`( ${parts.join('')} )`)
+        }
+
+        return items
+    }
+
+    _compare({ value, type = typeOf(value), operator = '='} = {}) {
+        if ( type !== 'number' ) value = `'${value}'`
+
+        return `${operator} ${value}`
+    }
+
+    _isSearch(obj) {
+        return Object.keys(obj).some((key) => key in this.comparators)
+    }
+
+    _parseTree(pointer, keys = []) {
+        let type = typeOf(pointer)
+
+        for ( let key in pointer ) {
+            let value = pointer[key]
+
+            if ( type === 'object' ) key = `'${key}'`
+
+            keys.push(key)
+            type = typeOf(value) // Ensures type matches the wrapper for keys
+
+            switch ( type ) {
+                case 'object':
+                    if ( this._isSearch(value) )
+                        return { keys, search: value }
+
+                case 'array':
+                case 'object':
+                    return this._parseTree(value, keys)
+
+                default:
+                    return { keys, search: { $eq: value } }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,64 @@
+# JSONB BUILDER
+
+`npm install jsonb-builder`
+
+```js
+import JsonbBuilder from 'jsonb-builder'
+
+let queries = new JsonbBuilder({ column: 'data' }).get({
+    something : { $like: '%word'},
+    equals    : { $eq: 'value'},
+    equals2   : 'value',
+
+    "some.thing[0]['!deeper!']": {
+        $lt: 5,
+        $gt: 2,
+    },
+})
+
+let comparisons = queries.map((query) => query.get())
+/*
+    [
+        [
+            "( data ->> 'something' ) LIKE '%word'"
+        ],
+        [
+            "( data ->> 'equals' ) = 'value'"
+        ],
+        [
+            "( data ->> 'equals2' ) = 'value'"
+        ],
+        [
+            "( data -> 'some' -> 'thing' -> 0 ->> '!deeper!' )::int < 5",
+            "( data -> 'some' -> 'thing' -> 0 ->> '!deeper!' )::int > 2"
+        ]
+    ]
+*/
+
+// use `asStatement` to exclude the comparison logic
+let statements = queries.map((query) => query.get({ asStatement: true }))
+/*
+    [
+        [
+            "( data ->> 'something' )"
+        ],
+        [
+            "( data ->> 'equals' )"
+        ],
+        [
+            "( data ->> 'equals2' )"
+        ],
+        [
+            "( data -> 'some' -> 'thing' -> 0 ->> '!deeper!' )"
+        ]
+    ]
+*/
+```
+
+
+### Search comparitors
+- `$gt` - `> 5` - greater than
+- `$lt` - `< 5` - lesser than
+- `$eq` - `= 5` - equal to
+- `$like` - `LIKE '%str%' (`%` signs must already exist in input)
+- more soon...
